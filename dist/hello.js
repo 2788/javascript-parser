bc856db40ec036f23b0d9f2833969a7107edbd26
duli <1033579059@qq.com>
2020-06-10 00:35:05 +0800

    none

--- dist/hello.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hello = void 0;
-function hello(a, b) {
-    console.log(a, b);
-}
-exports.hello = hello;
--- dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const hello_1 = require("./hello");
-hello_1.hello("d", "x");
